8adea9d4032b3cbbcdd0192f36fb6f765210d251
gameMedia <74588875+gameMedia@users.noreply.github.com>
2021-11-07 20:42:47 +0900

    first commit

---
+++ SIMPLEST_MMORPG_CLIENT/SIMPLEST_MMORPG_CLIENT/Console.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//ÄÜ¼ÖÃ¢. 
+#ifdef UNICODE
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console") 
+#else
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console") 
+#endif
